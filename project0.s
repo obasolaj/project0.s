009acb3d2bcc9fbc9a3a1f9cc31fc6239a012249
obasolaj <81383659+obasolaj@users.noreply.github.com>
2021-09-29 21:21:12 -0400

    Update project0.s

--- project0.s
+++ project0.s
@@ -6,4 +6,7 @@ main: # Start of code section
  li $v0, 4 # system call code for printing string = 4
  la $a0, out_string # load address of string
                       # to be printed into $a0
-  
+ syscall # call operating system to perform operation in $v0
+  # syscall takes its arguments from $a0, $a1, ...
+ lb $t1, 2($a0) # Read 'e' (the ASCII code) from memory
+ addi $t1, $t1, -32 # Change its ASCII code to 'E'
