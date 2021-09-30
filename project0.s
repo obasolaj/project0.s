107142e84db96aca9b422eccbd8f74a50525acfc
obasolaj <81383659+obasolaj@users.noreply.github.com>
2021-09-29 21:18:45 -0400

    Update project0.s

--- project0.s
+++ project0.s
@@ -2,3 +2,8 @@
  .data # Data declaration section
  out_string: .asciiz "\nHello, World!\n"
  .text # Assembly language instructions
+main: # Start of code section
+ li $v0, 4 # system call code for printing string = 4
+ la $a0, out_string # load address of string
+                      # to be printed into $a0
+  
