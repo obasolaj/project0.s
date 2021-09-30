9d74eecc1f40ab749c640596ebdbf142b8a16f77
obasolaj <81383659+obasolaj@users.noreply.github.com>
2021-09-29 21:22:02 -0400

    Update project0.s

--- project0.s
+++ project0.s
@@ -7,6 +7,10 @@ main: # Start of code section
  la $a0, out_string # load address of string
                       # to be printed into $a0
  syscall # call operating system to perform operation in $v0
-  # syscall takes its arguments from $a0, $a1, ...
+              # syscall takes its arguments from $a0, $a1, ...
  lb $t1, 2($a0) # Read 'e' (the ASCII code) from memory
  addi $t1, $t1, -32 # Change its ASCII code to 'E'
+ sb $t1, 2($a0) # Write the new ASCII code to memory
+ syscall # Print the string again
+ li $v0, 10
+ syscall # Exit program
